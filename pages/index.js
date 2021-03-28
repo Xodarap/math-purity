15ddd80745fcdbdfc08a091d8adee5944050ab9a
Ben West <Xodarap@users.noreply.github.com>
2021-03-27 18:24:38 -0700

    Cleanup

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,8 @@ import FormLabel from '@material-ui/core/FormLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import { Container, Divider, Link, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Footer from '../components/footer';
 
